1d6373a888b121e2e281acda052f3a2990c9bc24
hyzboy <hyzboy@gmail.com>
2018-10-23 19:55:06 +0800

    add field type define.

---
+++ FieldType.h
@@ -0,0 +1,57 @@
+﻿#ifndef HGL_DB_FIELD_TYPE_INCLUDE
+#define HGL_DB_FIELD_TYPE_INCLUDE
+
+namespace hgl
+{
+    namespace db
+    {
+        /**
+         * 字段基本类型枚举
+         */
+        enum FieldBaseType
+        {
+            fbtNone=0,
+
+            fbtBool,
+
+            fbtInt8,			fbtInt16,			fbtInt32,			fbtInt64,
+            fbtUint8,			fbtUint16,			fbtUint32,			fbtUint64,
+
+            fbtFloat,
+            fbtDouble,
+
+            fbtChar8,           ///<单字节字符
+            fbtChar16,          ///<双字节字符
+            fbtChar32,          ///<四字节字符
+
+            fbtEnd
+        };//enum FieldBaseType
+
+        /**
+         * 字段组合类型
+         */
+        enum FieldComboType
+        {
+            fctNone=0,
+
+            fctSingle,                  ///<单一数据
+            fctArray,                   ///<定长阵列
+            fctVarArray,                ///<变长阵列
+            fctStruct,                  ///<结构体，自由组成
+
+            fctEnd
+        };//enum FieldComboType
+
+        /**
+         * 字段类型
+         */
+        class FieldType
+        {
+            FieldBaseType base_type;    ///<基本类型
+            FieldComboType combo_type;  ///<组合类型
+
+        public:
+        };//class FieldType
+    }//namespace db
+}//namespace hgl
+#endif//HGL_DB_FIELD_TYPE_INCLUDE
---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+namespace db
+{
+    /**
+     * 字段类型
+    enum FieldType
+    {
+    };//
+}//namespace db
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
